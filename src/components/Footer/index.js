28287cab253c7a666b5cd8aba0d9f26b8654d4cc
khubayab <sshaikkhubayab@gmail.com>
2023-02-01 15:37:01 +0000

    second

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,21 @@
+import {VscGithubAlt} from 'react-icons/vsc'
+import {FaTwitter} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import './index.css'
+
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-container">
+      <h1 className="header-covid-logo">COVID19INDIA</h1>
+
+      <p className="footer-paragraph">
+        we stand with everyone fighting on the front lines
+      </p>
+      <div className="footer-social-logo">
+        <VscGithubAlt className="footer-logo" />
+        <FiInstagram className="footer-logo" />
+        <FaTwitter className="footer-logo" />
+      </div>
     </div>
   )
 }
